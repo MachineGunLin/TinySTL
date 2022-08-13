473f6e3740e970c73521faf270f88e4dd26c14a1
MachineGunLin <805435831@qq.com>
2022-08-13 17:55:55 +0800

    初始化 alloc 类成员

--- Alloc.h
+++ Alloc.h
@@ -55,6 +55,13 @@ namespace TinySTL {
         static void *allocate(size_t bytes);
         static void deallocate(void *ptr, size_t bytes);
         static void *reallocate(void *ptr, size_t old_sz, size_t new_sz);
+
+        char *alloc::start_free = 0;
+        char *alloc::end_free = 0;
+        size_t alloc::heap_size = 0;
+        alloc::obj *alloc::free_list[alloc::ENFreeLists::NFREELISTS] = {
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        }
     };
 }
 
